a3afbc65ebcb4d2a70f41c64b9ec7e6844ad937d
Lais Ribeiro Da Corte <lais.corte@telefonica.com>
2024-02-05 15:08:19 -0300

    javascript

---
+++ app.js
@@ -0,0 +1,29 @@
+alert("Boas Vindas");
+let vezes= 100;
+let numeroSecreto = parseInt(Math.random() * vezes + 1)
+console.log(numeroSecreto);
+let chute;
+let tentativas= 1;
+
+while(chute != numeroSecreto){
+    //vezes = prompt("Escolha ate que numero gostaria de adivinhar");
+    chute = prompt(`Escolha um número entre 1 e ${vezes}`);
+    if (chute == numeroSecreto){
+       break; 
+    }else{
+        if(chute > numeroSecreto){
+            alert(`O numero é menor que ${chute}`);
+        }else{
+            alert(`O numero é maior que ${chute}`);
+        }
+        tentativas++
+    }
+    
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você acertou o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!!`);
+//if(tentativas > 1){
+//    alert(`Você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativas!!`);
+//}else{
+//    alert(`Você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativa!!`);}
